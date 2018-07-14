edc66b3c7a1cedd552ceca4b249c3b669b6904b7
Sergey19940808 <alekseyserzh88@gmail.com>
2018-07-14 07:33:04 +0500

    init

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href=''>
+                                Корзина
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default App;
